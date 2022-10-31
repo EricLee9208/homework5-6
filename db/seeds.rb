5fb29a09dc21b76834b05d572e78b743751515ae
eric-lee <eric9208@hotmail.com>
2022-10-30 21:50:24 -0700

    mid point commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.destroy_all
+
+50.times do 
+    created_at = Faker::Date.backward(days: 365 * 5)
+   p =  Post.create(
+        title: Faker::Hacker.say_something_smart,
+        body: Faker::Lorem.sentence(word_count: 50),
+        created_at: created_at,
+        updated_at: created_at
+    )
+
+    if p.valid?
+        rand(1..5).times do 
+            Comment.create(
+            body: Faker::Hacker.say_something_smart,
+            post: p
+            )
+        end
+    end
+end
