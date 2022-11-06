641b4931c929de8f20f91ac31a36b29f02dc1f6e
eric-lee <eric9208@hotmail.com>
2022-11-06 01:12:52 -0800

    homework_6 initial commit

--- Gemfile
+++ Gemfile
@@ -36,10 +36,14 @@ gem "jbuilder"
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 gem 'webpacker', '~> 5.0'
 gem 'mini_racer', platforms: :ruby
 gem 'jquery-rails'
+
+gem 'cowsay', '~> 0.3.0'
+
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,23 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Post.destroy_all
+Comment.destroy_all
+User.destroy_all
+
+PASSWORD = "123"
+
+10.times do
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name}@#{last_name}.com",
+    password: PASSWORD,
+    )
+end
+
+users = User.all
 
 50.times do 
     created_at = Faker::Date.backward(days: 365 * 5)
@@ -14,15 +31,22 @@ Post.destroy_all
         title: Faker::Hacker.say_something_smart,
         body: Faker::Lorem.sentence(word_count: 50),
         created_at: created_at,
-        updated_at: created_at
+        updated_at: created_at,
+        user: users.sample
     )
 
     if p.valid?
         rand(1..5).times do 
             Comment.create(
             body: Faker::Hacker.say_something_smart,
-            post: p
+            post: p,
+            user: users.sample
             )
         end
     end
 end
+post = Post.all
+comment = Comment.all
+puts Cowsay.say("Generated #{post.count} posts", :frogs)
+puts Cowsay.say("Generated #{comment.count} comments", :frogs)
+puts Cowsay.say("Genereated #{users.count} users", :koala)
