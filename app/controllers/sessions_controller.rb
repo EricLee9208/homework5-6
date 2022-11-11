38c340b9b7e17f28a9237339178f871b7f623d59
eric-lee <eric9208@hotmail.com>
2022-11-10 21:30:32 -0800

    added more styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path, {notice: "Logged in"}
     else
-      render :new, {alert: "Wrong Email or password", status: 303}
+      flash[:alert] = "Wrong Email or password"
+      render :new, status: 303
     end
   end
 
